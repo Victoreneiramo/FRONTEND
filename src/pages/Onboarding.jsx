2fe0be69d8dbff7ec2db49e1870d4b113727ac4d
Victoreneiramo <victoreneiramo3@gmail.com>
2026-01-28 12:06:50 +0100

    Update Onboarding.jsx

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import heroImg from "../assets/images/doctor-patient.jpeg";
 import consultImg from "../assets/images/doctor-consult.jpeg";
 import aiImg from "../assets/images/ai-health.jpeg";
-import logoImg from "../assets/images/Logo.png";
+import logoImg from "../assets/images/NovaCareLogo.png";
 import backgroundImg from "../assets/images/background.png";
 
 export default function Onboarding() {
