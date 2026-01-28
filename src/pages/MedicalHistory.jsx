7b2f11118a5951e5f0e8db8d5c8d7ded7bf5f5a3
Victoreneiramo <victoreneiramo3@gmail.com>
2026-01-28 11:50:21 +0100

    Update MedicalHistory.jsx

--- src/pages/MedicalHistory.jsx
+++ src/pages/MedicalHistory.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import logoImg from "../assets/images/LOGO.jpeg";
+import logoImg from "../assets/images/Logo.png";
 
 function MedicalHistory() {
   const { user, logout } = useAuth();
